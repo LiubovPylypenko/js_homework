b92a1ca5bba0942308282b578368a17d6e9fbade
LiubovPylypenko <ljubovpylypenko@gmail.com>
2023-10-10 23:57:16 +0300

    HW re-worked

--- HW4.js
+++ HW4.js
@@ -1,17 +1,28 @@
-let age = prompt("Enter your age:");
+let age = prompt("Введіть ваш вік:");
 
 if (!isNaN(age))
 {
     if  ( age < 0 )
     {
-        console.log("Age can't be negative!");
+        console.log("Вік не може бути негативним!");
     }
     else
     {
-        console.log("Your age is " + age + " years. ");
+        switch (age % 10) {
+          case 1:
+            console.log("Ваш вік є " + age + " рік.");
+            break;
+          case 2:
+          case 3:
+          case 4:
+            console.log("Ваш вік є " + age + " роки.");
+            break;
+          default:
+            console.log("Ваш вік є " + age + " років.");
+        }       
     }
 }
 else
 {
-    console.log("Your age is not a number.");
+    console.log("Ви ввели вік який не не є числом");
 }
